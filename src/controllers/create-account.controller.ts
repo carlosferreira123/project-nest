65799ec545a4a1d22afa66c9ded544c9cfe250d4
Carlos Alisson <carlosalisson01@gmail.com>
2025-05-06 12:12:48 -0300

    commit

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,22 @@
+import {Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+ @Post()
+ @HttpCode(201)
+ async handle(@Body() body:any) {
+    const {name, email, password} = body
+   
+    
+    await this.prisma.user.create({
+       data: {
+           name,
+           email,
+           password,
+      }
+    })
+ }
+}
